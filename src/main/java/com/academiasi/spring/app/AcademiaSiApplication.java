19e0a19fecb24859394c3177ac4b73e7ab45a611
Michael <infoalexumg@gmail.com>
2020-03-23 13:50:22 -0600

    Creando el Proyecto en GitHub 23/03/2020

---
+++ src/main/java/com/academiasi/spring/app/AcademiaSiApplication.java
@@ -0,0 +1,13 @@
+package com.academiasi.spring.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademiaSiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademiaSiApplication.class, args);
+	}
+
+}
